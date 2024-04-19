9eacc53e621fb8e924a105a6dc964eaca903b214
Simon Ludwig <simon.ludwig@study.hs-duesseldorf.de>
2024-04-19 17:20:53 +0200

    added link to project detail page

--- app/components/ProjectPreview.tsx
+++ app/components/ProjectPreview.tsx
@@ -1,11 +1,12 @@
 import {Project} from ".prisma/client";
+import Link from "next/link";
 
 export default function ProjectPreview(project: Project) {
     return (
         <div>
             <h4>{project.title}</h4>
             <p>{project.subtitle}</p>
-            <a href="#">Learn more...</a>
+            <Link href={`/projects/${project.slug}`}>Learn more</Link>
         </div>
     );
 }
