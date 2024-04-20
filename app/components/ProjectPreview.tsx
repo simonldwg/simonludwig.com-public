b35fde0e54c493e1326014f96e8a9fb704ac99b6
Simon Ludwig <simon.ludwig@study.hs-duesseldorf.de>
2024-04-20 21:38:02 +0200

    fix corners being light

--- app/components/ProjectPreview.tsx
+++ app/components/ProjectPreview.tsx
@@ -5,11 +5,11 @@ import {ChevronRight} from "react-feather"
 
 export default function ProjectPreview(project: Project) {
   return (
-    <div className="bg-light text-title rounded-xl overflow-hidden">
+    <div className="text-title rounded-xl overflow-hidden">
       <img src={project.imageSmall} alt={`${project.title} project image`} className="w-full"/>
       <div className="p-4">
         <Heading3 colorClass="text-title">{project.title}</Heading3>
-        <p className="font-mono text-text text-sm mt-3 mb-3">{project.subtitle}</p>
+        <p className="font-mono bg-light text-text text-sm mt-3 mb-3">{project.subtitle}</p>
         <Link href={`/projects/${project.slug}`}><ChevronRight className="inline mr-1" />Learn more</Link>
       </div>
     </div>
