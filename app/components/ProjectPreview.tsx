355b91754a7291f381baafdf074df31e07205f4d
Simon Ludwig <simon.ludwig@study.hs-duesseldorf.de>
2024-04-20 22:42:18 +0200

    fixed corners not being rounded when not all items in a row are the same height

--- app/components/ProjectPreview.tsx
+++ app/components/ProjectPreview.tsx
@@ -5,9 +5,9 @@ import {ChevronRight} from "react-feather"
 
 export default function ProjectPreview(project: Project) {
   return (
-    <div className="text-title rounded-xl overflow-hidden">
-      <img src={project.imageSmall} alt={`${project.title} project image`} className="w-full"/>
-      <div className="p-4 bg-light">
+    <div className="text-title">
+      <img src={project.imageSmall} alt={`${project.title} project image`} className="w-full rounded-t-xl"/>
+      <div className="p-4 bg-light rounded-b-xl">
         <Heading3 colorClass="text-title">{project.title}</Heading3>
         <p className="font-mono text-text text-sm mt-3 mb-3">{project.subtitle}</p>
         <Link href={`/projects/${project.slug}`}><ChevronRight className="inline mr-1" />Learn more</Link>
