f22c82bcffbe0d75f31fc18a5e57ec068f65ec94
Simon Ludwig <simon.ludwig@study.hs-duesseldorf.de>
2024-04-20 13:37:36 +0200

    unified code style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,28 @@
-import './globals.css'
+import "./globals.css"
 
 export const metadata = {
-    title: 'Simon Ludwig',
-    description: '',
+  title: "Simon Ludwig",
+  description: "",
 }
 
 function renderFontCSS() {
-    if(process.env.FONT_CSS_URL) {
-        return <link rel="stylesheet" href={process.env.FONT_CSS_URL} />
-    }
-    return null
+  if (process.env.FONT_CSS_URL) {
+    return <link rel="stylesheet" href={process.env.FONT_CSS_URL}/>
+  }
+  return null
 }
 
-export default function RootLayout({
-  children,
-}: {
+export default function RootLayout({ children, }: {
   children: React.ReactNode;
 }) {
   return (
-      <html lang="en">
-      <head>
-          { renderFontCSS() }
-      </head>
-      <body>{children}</body>
-      </html>
-  );
+    <html lang="en">
+    <head>
+      {renderFontCSS()}
+    </head>
+    <body>
+      {children}
+    </body>
+    </html>
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,26 @@
-import Link from "next/link";
-import {getAllProjects} from "@/lib/projectManager";
-import ProjectPreview from "@/app/components/ProjectPreview";
+import Link from "next/link"
+import {getAllProjects} from "@/lib/projectManager"
+import ProjectPreview from "@/app/components/ProjectPreview"
 
-export const runtime = 'edge'
+export const runtime = "edge"
 
 const Home = async () => {
-    const projects = await getAllProjects()
+  const projects = await getAllProjects()
 
-    return (
-        <div>
-            <h1>Home</h1>
-            <p>Hello World! This is the Home page</p>
-            <div>
-                {
-                    projects.map((project) => ProjectPreview(project))
-                }
-            </div>
-            <p>
-                Visit the <Link href="/about">About</Link> page.
-            </p>
-        </div>
-    );
-};
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Hello World! This is the Home page</p>
+      <div>
+        {
+          projects.map((project) => ProjectPreview(project))
+        }
+      </div>
+      <p>
+        Visit the <Link href="/about">About</Link> page.
+      </p>
+    </div>
+  )
+}
 
-export default Home;
+export default Home
