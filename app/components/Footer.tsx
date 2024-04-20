af74ad1d3ba5a12862e701e5e3f9c7e60745d54e
Simon Ludwig <simon.ludwig@study.hs-duesseldorf.de>
2024-04-20 22:48:06 +0200

    improved footer padding

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import {GitHub, Linkedin} from "react-feather"
 
 export default function Footer() {
   return (
-    <footer className="max-w-screen-2xl mx-auto p-2 md:p-4 text-center text-sm text-text sm:grid sm:grid-cols-3 w-full">
+    <footer className="max-w-screen-2xl mx-auto p-4 text-center text-sm text-text sm:grid sm:grid-cols-3 w-full">
       <div className="sm:text-left mb-3 sm:mb-0">
         <Link href="#" className="mr-4">Privacy Policy</Link>
         <Link href="#">Imprint</Link>
@@ -16,7 +16,7 @@ export default function Footer() {
           <Linkedin className="inline w-7"/>
         </Link>
       </div>
-      <div className="sm:text-right mb-3 sm:mb-0">
+      <div className="sm:text-right">
         <p>Built with <Link href="https://nextjs.org/">Next.js</Link> on <Link href="https://pages.cloudflare.com/">Cloudflare Pages</Link>.</p>
       </div>
     </footer>
