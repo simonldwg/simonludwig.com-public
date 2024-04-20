f0d2d251bb6871d7e9a20341aeadeaafac509192
Simon Ludwig <simon.ludwig@study.hs-duesseldorf.de>
2024-04-20 21:40:17 +0200

    fix fix corners being light

--- app/components/ProjectPreview.tsx
+++ app/components/ProjectPreview.tsx
@@ -7,9 +7,9 @@ export default function ProjectPreview(project: Project) {
   return (
     <div className="text-title rounded-xl overflow-hidden">
       <img src={project.imageSmall} alt={`${project.title} project image`} className="w-full"/>
-      <div className="p-4">
+      <div className="p-4 bg-light">
         <Heading3 colorClass="text-title">{project.title}</Heading3>
-        <p className="font-mono bg-light text-text text-sm mt-3 mb-3">{project.subtitle}</p>
+        <p className="font-mono text-text text-sm mt-3 mb-3">{project.subtitle}</p>
         <Link href={`/projects/${project.slug}`}><ChevronRight className="inline mr-1" />Learn more</Link>
       </div>
     </div>
