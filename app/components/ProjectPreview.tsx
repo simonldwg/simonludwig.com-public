014d770a604702e5127277ed5a9baed54590dd85
Simon Ludwig <simon.ludwig@study.hs-duesseldorf.de>
2024-04-20 22:53:51 +0200

    added border to project preview

--- app/components/ProjectPreview.tsx
+++ app/components/ProjectPreview.tsx
@@ -7,7 +7,7 @@ export default function ProjectPreview(project: Project) {
   return (
     <div className="text-title">
       <img src={project.imageSmall} alt={`${project.title} project image`} className="w-full rounded-t-xl"/>
-      <div className="p-4 bg-light rounded-b-xl">
+      <div className="p-4 bg-light rounded-b-xl border-t-gray-300 border-t-2">
         <Heading3 colorClass="text-title">{project.title}</Heading3>
         <p className="font-mono text-text text-sm mt-3 mb-3">{project.subtitle}</p>
         <Link href={`/projects/${project.slug}`}><ChevronRight className="inline mr-1" />Learn more</Link>
