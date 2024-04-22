32dc7af68cc0acb80aab44bc5180e568c8b6a9ec
Simon Ludwig <simon.ludwig@study.hs-duesseldorf.de>
2024-04-22 23:10:25 +0200

    added scroll fade-in animation

--- app/components/ProjectPreview.tsx
+++ app/components/ProjectPreview.tsx
@@ -1,11 +1,18 @@
+"use client"
+
 import {Project} from ".prisma/client"
 import Link from "next/link"
 import Heading3 from "@/app/components/typography/Heading3"
 import {ChevronRight} from "react-feather"
+import {useRef} from "react"
+import {useIsVisible} from "@/lib/animationHelpers"
 
 export default function ProjectPreview(project: Project) {
+  const projectPreviewRef = useRef<any>()
+  const projectPreviewVisible = useIsVisible(projectPreviewRef)
+
   return (
-    <div className="text-gray-900">
+    <div ref={projectPreviewRef} className={`text-gray-900 transition-all transform ease-in duration-600 ${projectPreviewVisible ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10"}`}>
       <img src={project.imageSmall} alt={`${project.title} project image`} className="w-full rounded-t-xl"/>
       <div className="p-4 bg-gray-50 rounded-b-xl border-t-gray-300 border-t-2">
         <Heading3 colorClass="text-gray-900">{project.title}</Heading3>
